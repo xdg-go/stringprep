78002072c8c677546d27b71b0d7903c990109c55
David Golden <xdg@xdg.me>
2018-02-20 17:05:16 -0500

    Put test data into test functions

--- saslprep_test.go
+++ saslprep_test.go
@@ -6,35 +6,33 @@ import (
 	"testing"
 )
 
-type saslCase struct {
-	label string
-	in    string
-	out   string
-	err   error
-}
-
-var saslTests = []saslCase{
-	{label: "soft hyphen", in: "I\u00ADX", out: "IX", err: nil},
-	{label: "non ASCII space", in: "I\u2000X", out: "I X", err: nil},
-	{label: "no transform", in: "user", out: "user", err: nil},
-	{label: "case preserve", in: "USER", out: "USER", err: nil},
-	{label: "8859-1 to NFKC", in: "\u00AA", out: "a", err: nil},
-	{label: "NFKC", in: "\u2168", out: "IX", err: nil},
-	{
-		label: "prohibited",
-		in:    "\u0007",
-		out:   "",
-		err:   Error{Msg: errProhibited, Rune: '\u0007'},
-	},
-	{
-		label: "bidi not ok",
-		in:    "\u0627\u0031",
-		out:   "",
-		err:   Error{Msg: errLastRune, Rune: '\u0031'},
-	},
-}
-
 func TestSASLprep(t *testing.T) {
+	saslTests := []struct {
+		label string
+		in    string
+		out   string
+		err   error
+	}{
+		{label: "soft hyphen", in: "I\u00ADX", out: "IX", err: nil},
+		{label: "non ASCII space", in: "I\u2000X", out: "I X", err: nil},
+		{label: "no transform", in: "user", out: "user", err: nil},
+		{label: "case preserve", in: "USER", out: "USER", err: nil},
+		{label: "8859-1 to NFKC", in: "\u00AA", out: "a", err: nil},
+		{label: "NFKC", in: "\u2168", out: "IX", err: nil},
+		{
+			label: "prohibited",
+			in:    "\u0007",
+			out:   "",
+			err:   Error{Msg: errProhibited, Rune: '\u0007'},
+		},
+		{
+			label: "bidi not ok",
+			in:    "\u0627\u0031",
+			out:   "",
+			err:   Error{Msg: errLastRune, Rune: '\u0031'},
+		},
+	}
+
 	for _, c := range saslTests {
 		t.Run(c.label, func(t *testing.T) {
 			got, err := SASLprep.Prepare(c.in)
